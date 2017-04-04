50cbdea4e982b20062d4afaaf77a1b39a226175e
Robbie Robinson <everettlrobinson@gmail.com>
2017-04-04 09:06:29 +0200

    refactor Form for edit document
    refactored the Form used to edit a document to use Grommet instead of
    react-bootstrap

--- app/containers/UserDetailsContainer.js
+++ app/containers/UserDetailsContainer.js
@@ -2,12 +2,12 @@
  * Created by everettrobinson on 2/8/17.
  */
 import { Grid, Row, Thumbnail, Col, Button, MenuItem, ButtonToolbar, DropdownButton, Popover, Tooltip, Modal, OverlayTrigger,
-    Form, FormGroup,FormControl, ControlLabel} from 'react-bootstrap'
+     FormGroup,FormControl, ControlLabel} from 'react-bootstrap'
 import {Table} from 'react-bootstrap';
 import TextInput from '../common/TextInput';
 import Comment from '../common/Comment';
 import UserDetails from '../components/UserDetails'
-import GForm from 'grommet/components/Form'
+import Form from 'grommet/components/Form'
 import FormField from 'grommet/components/FormField'
 import GromTextInput from 'grommet/components/TextInput'
 import DateTime from 'grommet/components/DateTime';
@@ -18,6 +18,8 @@ import Footer from 'grommet/components/Footer';
 import Label from 'grommet/components/Label';
 import Select from 'grommet/components/Select';
 import GromButton from 'grommet/components/Button';
+import Fieldset from 'grommet/components/FormFields';
+
 
 
 
@@ -67,41 +69,38 @@ class UserDetailsContainer extends React.Component {
         this.handleDatePickerChange = this.handleDatePickerChange.bind(this);
         this.editDocument = this.editDocument.bind(this);
         this.saveDocument = this.saveDocument.bind(this);
-        this.getDocumentState = this.getDocumentState(this);
-
     }
 
   close() {
-      this.setState({showModal: false})
+      this.setState({showForm: false})
       }
 
     updateDocumentState(event){
-        event.preventDefault();
-        console.log('updating document state', event.target.name);
         const field = event.target.name;
         const document = this.state.editDocument;
-        document[field] = event.target.value;
+        event.option !== '' ?
+            document[field] = event.option
+        :
+            document[field] = event.target.value;
+
         return this.setState({
             editDocument: document
         })
     }
 
-    getDocumentState() {
-        return this.state.editDocument;
-    }
 
     handleDatePickerChange(value) {
         console.log("inside handleDatePickerChange", value);
         const document = this.state.editDocument;
+        const field = 'due_date';
+        document[field] = value;
 
-        this.setState({
-            document_due_date: value
-
-        })
+        return this.setState({
+            editDocument: document
+            })
     }
 
     editDocument(document){
-        console.log('document ID', document)
         this.setState({
             editDocument: document,
             showModal: false,
@@ -124,58 +123,74 @@ class UserDetailsContainer extends React.Component {
                 flush={false}
                 onClose={() => {this.setState({showForm: false})}}>
 
-                <GForm pad="medium">
+                <Form pad="medium">
+
                     <Header>
                         <Heading>Edit Document</Heading>
                     </Header>
 
                     <Label>Reference ID: {this.state.editDocument._id}</Label>
-
+                <fieldset>
                     <FormField label="Name">
                         <GromTextInput defaultValue={this.state.editDocument.name} onDOMChange={this.updateDocumentState}/>
                     </FormField>
+               </fieldset>
+
+                <fieldset>
                     <FormField label="Status">
                     <Select placeHolder='None'
                             inline={false}
                             options={['Complete', 'Uploaded', 'Processing', 'Missing']}
                             value={this.state.editDocument.status}
+                            name="status"
+                            onChange={this.updateDocumentState}
                              />
                         </FormField>
+                  </fieldset>
+                    <fieldset>
 
                     <FormField label="Due Date">
                         <DateTime
                             id="dueDate"
-                            name="dueDate"/>
+                            name="due_date"
+                            value={this.state.editDocument.due_date}
+                            onChange={this.handleDatePickerChange}/>
                     </FormField>
+                  </fieldset>
 
-                    <FormField label="Condition">
+                    <fieldset>
+                        <FormField label="Condition">
                         <Select placeHolder='None'
                                 inline={false}
+                                name="condition"
                                 options={['Excellent', 'Good', 'Poor', 're-upload']}
                                 value={this.state.editDocument.condition}
+                                onChange={this.updateDocumentState}
                         />
                     </FormField>
+                  </fieldset>
 
 
                     <Footer pad={{"vertical": "medium"}}>
+
                         <GromButton label='Save'
-                                href='#'
                                 primary={false}
                                 secondary={false}
                                 accent={false}
                                 plain={false}
-                                type='submit' />
+                                type='submit'
+                                onClick={this.saveDocument}/>
 
                         <GromButton label='Cancel'
-                                href='#'
                                 primary={false}
                                 secondary={true}
                                 accent={false}
                                 plain={false}
-                                type='submit' />
-                    </Footer>
+                                type='button'
+                                onClick={this.close}/>
 
-                </GForm>
+                    </Footer>
+                </Form>
 
                 </Layer>
             : null;
@@ -202,96 +217,7 @@ class UserDetailsContainer extends React.Component {
 
                 <UserDetails user = {this.props.user}  documents = {documents} />
                 {layer}
-
-                <Modal show={this.state.showModal} onHide={this.close}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Edit Document</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <div>
-                            <form>
-                                <label>Reference ID: {this.state.editDocument._id}</label>
-
-
-                            <TextInput
-                                name="name"
-                                label="Name"
-                                value={this.state.editDocument.name}
-                                onChange={this.updateDocumentState}
-                                />
-
-
-
-                                <div className="form-group">
-                                    <label htmlFor={this.state.editDocument.status}>Status</label>
-                                    <select className="form-control" id={this.state.editDocument.status} onChange="">
-                                        <option selected disabled>{this.state.editDocument.status}</option>
-                                        <option>complete</option>
-                                        <option>uploaded</option>
-                                        <option>processing</option>
-                                        <option>missing</option>
-                                    </select>
-                                </div>
-
-                                <div className="form-group">
-                                    <label htmlFor={this.state.editDocument.due_date}>Due Date</label>
-
-                                </div>
-
-                                <div className="form-group">
-                                    <label htmlFor={this.state.editDocument.condition}>Condition</label>
-                                    <select className="form-control" id={this.state.editDocument.condition} onChange="">
-                                        <option selected disabled>{this.state.editDocument.condition}</option>
-                                        <option>excellent</option>
-                                        <option>good</option>
-                                        <option>poor</option>
-                                        <option>re-upload</option>
-                                    </select>
-                                </div>
-
-                                <div>
-                                    <Comment comments = {this.state.doc_comments}/>
-
-                                </div>
-                                </form>
-                            </div>
-
-
-
-
-                        <table className="table">
-                            <thead>
-                            <tr>
-                                <th>Field</th>
-                                <th>Value</th>
-                            </tr>
-
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <td>Comments</td>
-                                <td>{this.state.editDocument.comments}</td>
-
-                            </tr>
-
-                            </tbody>
-                        </table>
-
-
-
-
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button bsStyle="primary" onClick={() => this.saveDocument(document)}>Save</Button>
-                        <Button onClick={this.close}>Cancel</Button>
-                    </Modal.Footer>
-
-
-
-                </Modal>
-
-
-
+                
             </div>
         )
 
